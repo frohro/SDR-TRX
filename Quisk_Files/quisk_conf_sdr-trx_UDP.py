e3429405ec46761e4f3f822b4acbe743a12b49f0
frohro <rob.frohne@wallawalla.edu>
2024-04-09 04:56:46 +0000

    Apr 8, 2024, 9:56 PM

--- Quisk_Files/quisk_conf_sdr-trx_UDP.py
+++ Quisk_Files/quisk_conf_sdr-trx_UDP.py
@@ -100,10 +100,10 @@ class Hardware(BaseHardware):
         time.sleep(2)
         # self.get_parameter("TX_FREQ, 14074000")
         # Poll for version. Should probably confirm the response on this.
-        version = str(self.get_parameter("VER"))  # The way the firmware is now, this sets it up to use the UART.
-        print(version)
+        # version = str(self.get_parameter("VER"))  # The way the firmware is now, this sets it up to use the UART.
+        # print(version)
         # Return an informative message for the config screen
-        t = str(version) + ". Capture from sound card %s." % self.conf.name_of_sound_capt
+        # t = str(version) + ". Capture from sound card %s." % self.conf.name_of_sound_capt
         return t
 
     def close(self):
