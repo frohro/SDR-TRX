65c1e0aba05b5c21cc43e9d51f240e36ac09c982
frohro <rob.frohne@wallawalla.edu>
2024-04-16 05:21:47 -0700

    Working on multicore.

--- src/Basic_Testing_UDP_Data_Stream.ino
+++ src/Basic_Testing_UDP_Data_Stream.ino
@@ -29,12 +29,28 @@ const size_t BUFFER_SIZE = 1470; // Assuming the MTU is 1500 bytes...
 char buffer[BUFFER_SIZE];
 const char *udpAddress = "192.168.1.101"; // Put your laptop IP here.
 const unsigned int udpPort = 12345;
+uint32_t start_time;
+uint32_t total_time = 0;
+uint32_t n = 0;
 
 void loop()
 {
     // Send the buffer via UDP over and over again.
     // Read the data rate using the Ubuntu System Monitor.
-    udp.beginPacket(udpAddress, udpPort);
-    udp.write((const uint8_t *)buffer, BUFFER_SIZE);
-    udp.endPacket();
+    udp.beginPacket(udpAddress, udpPort);  // Takes about 7 us.
+
+    start_time = micros();
+    udp.write((const uint8_t *)buffer, BUFFER_SIZE);  // Takes about 810 us.
+  
+    udp.endPacket();  // Takes about 670 us
+    int time_elapsed = micros() - start_time;
+    total_time += time_elapsed;
+    if (n++ == 5000) 
+    {
+        Serial.printf("Time per statement: %f\n", (float)total_time / n);
+        start_time = micros();
+        total_time = 0;
+        n = 0;
+    }
+    
 }
--- test/Multicore_Test_UDP_I2S.cpp
+++ test/Multicore_Test_UDP_I2S.cpp
@@ -2,13 +2,12 @@
 * Project Name: Multicore_Test_UDP_I2S
 * File Name: Multicore_Test_UDP_I2S.cpp
 * This is a test of the multicore Arudino Pico librory for the Pico W 
-* to use both cors and a ping pong buffer to send data over UDP from the I2S.
+* to use both cors and a circular queue of buffers to send data over UDP from the I2S
 * using the PCM1808.
-*/
-
-/*
+ *
  * This Pico W program will connect to the PCM1808 via I2S and read the data it RATE samples per second.
- * It then sends the dato over WIFI using UDP.  
+ * It then sends the dato over WIFI using UDP. 
+ * 
  *
  * Author: Rob Frohne, KL7NA, with help from Perplexity and Github Copilot.
  * 4/9/2024
@@ -18,13 +17,15 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <si5351.h>
+#include <atomic>  // Do we need this?
 
 #ifndef STASSID
 #define STASSID Frohne - 2.4GHz
 #endif
 
 // Use #define for common constants to both cores.
-#define RATE 96000
+#define RATE 48000  // To do 96000, we need to use 24 bits instead of 32. 
+// For now we will use 32 bits for debugging ease.
 #define MCLK_MULT 256 // 384 for 48 BCK per frame,  256 for 64 BCK per frame
 
 I2S i2s(INPUT);
@@ -33,63 +34,105 @@ WiFiUDP udp;
 const char *udpAddress = "192.168.1.101"; // Put your laptop IP here.
 const unsigned int udpPort = 12345;
 
-const uint BUFFER_SIZE = 2*sizeof(int32_t)*180 + sizeof(u_int32_t); // Should be 1444 bytes. 
-char bufferA[BUFFER_SIZE];
-char bufferB[BUFFER_SIZE];
-char *currentBuffer = bufferA;
-char *sendBuffer = bufferB;
-int bufferIndex = 4;  // Start at 4 to leave room for the packet number.
-volatile bool dataReady = false;
+#define BUFFER_SIZE 1468 // 183 samples of 4 bytes + 4 bytes for the packet number
+#define QUEUE_SIZE 9
+
+class CircularBufferQueue {  // Need to use rp2040.fifo for this.
+private:
+    char buffers[QUEUE_SIZE][BUFFER_SIZE];
+    std::atomic<int> fillIndex;  // Index for filling the buffer
+    std::atomic<int> emptyIndex; // Index for emptying the buffer
+
+public:
+    // Constructor
+    CircularBufferQueue() : fillIndex(0), emptyIndex(0) {}
+
+    // Get a pointer to the next buffer to be filled
+    char* getNextFillBuffer() {
+        // Check if the fill index is about to overtake the empty index
+        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex) {
+            return buffers[fillIndex % QUEUE_SIZE];
+        } else {
+            return nullptr; // Return null if the buffer is full
+        }
+    }
+
+    // Move to the next buffer to be filled
+    void moveToNextFillBuffer() {
+        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex) {
+            fillIndex = (fillIndex + 1) % QUEUE_SIZE;
+        }
+    }
+
+    // Get a pointer to the next buffer to be emptied
+    char* getNextEmptyBuffer() {
+        // Check if the empty index is about to overtake the fill index
+        if (emptyIndex != fillIndex) {
+            return buffers[emptyIndex % QUEUE_SIZE];
+        } else {
+            return nullptr; // Return null if the buffer is empty
+        }
+    }
+
+    // Move to the next buffer to be emptied
+    void moveToNextEmptyBuffer() {
+        if (emptyIndex != fillIndex) {
+            emptyIndex = (emptyIndex + 1) % QUEUE_SIZE;
+        }
+    }
+};
+
+class BufferFiller {
+private:
+    CircularBufferQueue& queue;
+
+public:
+    BufferFiller(CircularBufferQueue& q) : queue(q) {}
+
+    void fillBuffer() {
+        char* buffer = queue.getNextFillBuffer();
+        if (buffer != nullptr) {  // There is space to fill the buffer with data 
+            static int32_t r, l;  // Static for a tiny boost in speed.
+            uint32_t bufferIndex = 4;  // Leave four bytes for the packet number.
+            i2s.read32(&l, &r); // Read the next l and r values (blocking)
+            l = l << 9;  // These should be 8 I think, but this works and 8 does not.
+            r = r << 9;  // https://github.com/earlephilhower/arduino-pico/issues/2037
+            memcpy(buffer + bufferIndex, &l, sizeof(int32_t)); // These are sending 2 32 bit integers per sample.
+            bufferIndex += sizeof(int32_t);
+            memcpy(buffer + bufferIndex, &r, sizeof(int32_t));
+            bufferIndex += sizeof(int32_t);  
+            if (bufferIndex == BUFFER_SIZE)
+            {  
+                memcpy(buffer, &packet_number, sizeof(int32_t)); // Before you reinitialize.   
+                packet_number++;
+                    queue.moveToNextFillBuffer();
+                }
+            }
+};
+
+class BufferEmptyer {
+private:
+    CircularBufferQueue& queue;
+
+public:
+    BufferEmptyer(CircularBufferQueue& q) : queue(q) {}
+
+    void emptyBuffer() {
+        char* buffer = queue.getNextEmptyBuffer();
+        if (buffer != nullptr) {
+            udp.beginPacket(udpAddress, udpPort);
+            udp.write((const uint8_t *)&buffer, BUFFER_SIZE);
+            udp.endPacket();
+            queue.moveToNextEmptyBuffer();
+        }
+    }
+};
+
+CircularBufferQueue bufferQueue;
 uint32_t packet_number = 0;
 
-void i2sDataReceived()
-{   // This should run on Core1.
-    // Serial.println("Data received");
-    if (i2s.read() < 2) return;  // If there is no data, return.
-    static int32_t r, l;  // Static for a tiny boost in speed.
-    i2s.read32(&l, &r); // Read the next l and r values
-    l = l << 9;
-    r = r << 9;
-    // Fill the current buffer with data
-    // Copy the binary data of l and r to the current buffer
-    memcpy(currentBuffer + bufferIndex, &l, sizeof(int32_t));
-    bufferIndex += sizeof(int32_t);
-    memcpy(currentBuffer + bufferIndex, &r, sizeof(int32_t));
-    bufferIndex += sizeof(int32_t);  // Separate increments could be sped up by using a single increment
-    // and pre-computing the number to increment by.  (Maybe the compiler optimizes this already.)
-    // Sending ASCII like this is a problem, because once in a while the number may not require
-    // the same number of characters.  This will cause a problem.  We need to send the data in binary.
-
-    // If the buffer is full, swap the buffers and add the packet number
-    if (bufferIndex == BUFFER_SIZE)
-    {  // Send the packet number, swap the buffers, and set the flag.
-        memcpy(currentBuffer, &packet_number, sizeof(int32_t)); // Before you reinitialize.
-        char *temp = currentBuffer;  // Swap the buffers
-        currentBuffer = sendBuffer;
-        sendBuffer = temp;
-        bufferIndex = 0;   // Reset the buffer index
-        packet_number++;   // Increment the packet number
-        dataReady = true;  // Set the flag to indicate data is ready to be sent
-    }
-}
-
-void setup()
-{   // This should run on Core0.  It is the WIFI and UDP setup.
-    Serial.begin();
-    WiFi.begin("Frohne-2.4GHz");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(10000);
-        Serial.println("Trying to connect to WiFi...");
-    }
-    Serial.println("WiFi connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-    udp.begin(udpPort);
-}
-
 void setup1()
-{   // This should run on Core1.  It is the I2S setup.
+{   // This runs on Core1.  It is the I2S setup.
     Serial.begin();
     i2s.setDATA(2); // These are the pins for the data on the SDR-TRX
     i2s.setBCLK(0);
@@ -100,24 +143,17 @@ void setup1()
     i2s.setFrequency(RATE);
     i2s.setMCLKmult(MCLK_MULT);
     i2s.setBuffers(32, 0, 0);
-     // Set the callback function
-    i2s.onReceive(i2sDataReceived);
     i2s.begin();
 }
 
 void loop()
-{
-    // If data is ready, send it over UDP
-    if (dataReady)
-    {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.write((const uint8_t *)sendBuffer, BUFFER_SIZE);
-        udp.endPacket();
-        dataReady = false;
-    }
+{   // This should run on Core0.  It is the UDP loop.
+    static BufferEmptyer emptyer(bufferQueue);
+    emptyer.emptyBuffer(); // Empty the buffer
 }
 
 void loop1()
-{
-    // Your other main loop code goes here...
+{   // This should run on Core1.  It is the I2S loop.
+    static BufferFiller filler(bufferQueue);
+    filler.fillBuffer(); // Fill the buffer
 }
