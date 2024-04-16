1463014f2f82d9a3a028ca0d057485b4a3d245ef
frohro <rob.frohne@wallawalla.edu>
2024-04-16 12:33:05 +0000

    Apr 16, 2024, 5:33 AM

--- src/Multicore_Test_UDP_I2S.cpp
+++ src/Multicore_Test_UDP_I2S.cpp
@@ -1,13 +1,13 @@
 /*
-* Project Name: Multicore_Test_UDP_I2S
-* File Name: Multicore_Test_UDP_I2S.cpp
-* This is a test of the multicore Arudino Pico librory for the Pico W 
-* to use both cors and a circular queue of buffers to send data over UDP from the I2S
-* using the PCM1808.
+ * Project Name: Multicore_Test_UDP_I2S
+ * File Name: Multicore_Test_UDP_I2S.cpp
+ * This is a test of the multicore Arudino Pico librory for the Pico W
+ * to use both cors and a circular queue of buffers to send data over UDP from the I2S
+ * using the PCM1808.
  *
  * This Pico W program will connect to the PCM1808 via I2S and read the data it RATE samples per second.
- * It then sends the dato over WIFI using UDP. 
- * 
+ * It then sends the dato over WIFI using UDP.
+ *
  *
  * Author: Rob Frohne, KL7NA, with help from Perplexity and Github Copilot.
  * 4/9/2024
@@ -17,14 +17,14 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <si5351.h>
-#include <atomic>  // Do we need this?
+#include <atomic> // Do we need this?
 
 #ifndef STASSID
 #define STASSID Frohne - 2.4GHz
 #endif
 
 // Use #define for common constants to both cores.
-#define RATE 48000  // To do 96000, we need to use 24 bits instead of 32. 
+#define RATE 48000 // To do 96000, we need to use 24 bits instead of 32.
 // For now we will use 32 bits for debugging ease.
 #define MCLK_MULT 256 // 384 for 48 BCK per frame,  256 for 64 BCK per frame
 
@@ -37,7 +37,8 @@ const unsigned int udpPort = 12345;
 #define BUFFER_SIZE 1468 // 183 samples of 4 bytes + 4 bytes for the packet number
 #define QUEUE_SIZE 9
 
-class CircularBufferQueue {  // Need to use rp2040.fifo for this.
+class CircularBufferQueue
+{ // Need to use rp2040.fifo for this.
 private:
     char buffers[QUEUE_SIZE][BUFFER_SIZE];
     std::atomic<int> fillIndex;  // Index for filling the buffer
@@ -48,78 +49,97 @@ public:
     CircularBufferQueue() : fillIndex(0), emptyIndex(0) {}
 
     // Get a pointer to the next buffer to be filled
-    char* getNextFillBuffer() {
+    char *getNextFillBuffer()
+    {
         // Check if the fill index is about to overtake the empty index
-        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex) {
+        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex)
+        {
             return buffers[fillIndex % QUEUE_SIZE];
-        } else {
+        }
+        else
+        {
             return nullptr; // Return null if the buffer is full
         }
     }
 
     // Move to the next buffer to be filled
-    void moveToNextFillBuffer() {
-        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex) {
+    void moveToNextFillBuffer()
+    {
+        if ((fillIndex + 1) % QUEUE_SIZE != emptyIndex)
+        {
             fillIndex = (fillIndex + 1) % QUEUE_SIZE;
         }
     }
 
     // Get a pointer to the next buffer to be emptied
-    char* getNextEmptyBuffer() {
+    char *getNextEmptyBuffer()
+    {
         // Check if the empty index is about to overtake the fill index
-        if (emptyIndex != fillIndex) {
+        if (emptyIndex != fillIndex)
+        {
             return buffers[emptyIndex % QUEUE_SIZE];
-        } else {
+        }
+        else
+        {
             return nullptr; // Return null if the buffer is empty
         }
     }
 
     // Move to the next buffer to be emptied
-    void moveToNextEmptyBuffer() {
-        if (emptyIndex != fillIndex) {
+    void moveToNextEmptyBuffer()
+    {
+        if (emptyIndex != fillIndex)
+        {
             emptyIndex = (emptyIndex + 1) % QUEUE_SIZE;
         }
     }
 };
 
-class BufferFiller {
+class BufferFiller
+{
 private:
-    CircularBufferQueue& queue;
+    CircularBufferQueue &queue;
 
 public:
-    BufferFiller(CircularBufferQueue& q) : queue(q) {}
+    BufferFiller(CircularBufferQueue &q) : queue(q) {}
 
-    void fillBuffer() {
-        char* buffer = queue.getNextFillBuffer();
-        if (buffer != nullptr) {  // There is space to fill the buffer with data 
-            static int32_t r, l;  // Static for a tiny boost in speed.
-            uint32_t bufferIndex = 4;  // Leave four bytes for the packet number.
-            i2s.read32(&l, &r); // Read the next l and r values (blocking)
-            l = l << 9;  // These should be 8 I think, but this works and 8 does not.
-            r = r << 9;  // https://github.com/earlephilhower/arduino-pico/issues/2037
+    void fillBuffer()
+    {
+        char *buffer = queue.getNextFillBuffer();
+        if (buffer != nullptr)
+        {                                                      // There is space to fill the buffer with data
+            static int32_t r, l, packet_number = 0;            // Static r, l for a tiny boost in speed.
+            uint32_t bufferIndex = 4;                          // Leave four bytes for the packet number.
+            i2s.read32(&l, &r);                                // Read the next l and r values (blocking)
+            l = l << 9;                                        // These should be 8 I think, but this works and 8 does not.
+            r = r << 9;                                        // https://github.com/earlephilhower/arduino-pico/issues/2037
             memcpy(buffer + bufferIndex, &l, sizeof(int32_t)); // These are sending 2 32 bit integers per sample.
             bufferIndex += sizeof(int32_t);
             memcpy(buffer + bufferIndex, &r, sizeof(int32_t));
-            bufferIndex += sizeof(int32_t);  
+            bufferIndex += sizeof(int32_t);
             if (bufferIndex == BUFFER_SIZE)
-            {  
-                memcpy(buffer, &packet_number, sizeof(int32_t)); // Before you reinitialize.   
+            {
+                memcpy(buffer, &packet_number, sizeof(int32_t)); // Before you reinitialize.
                 packet_number++;
-                    queue.moveToNextFillBuffer();
-                }
+                queue.moveToNextFillBuffer();
             }
+        }
+    };
 };
 
-class BufferEmptyer {
+class BufferEmptyer
+{
 private:
-    CircularBufferQueue& queue;
+    CircularBufferQueue &queue;
 
 public:
-    BufferEmptyer(CircularBufferQueue& q) : queue(q) {}
+    BufferEmptyer(CircularBufferQueue &q) : queue(q) {}
 
-    void emptyBuffer() {
-        char* buffer = queue.getNextEmptyBuffer();
-        if (buffer != nullptr) {
+    void emptyBuffer()
+    {
+        char *buffer = queue.getNextEmptyBuffer();
+        if (buffer != nullptr)
+        {
             udp.beginPacket(udpAddress, udpPort);
             udp.write((const uint8_t *)&buffer, BUFFER_SIZE);
             udp.endPacket();
@@ -132,7 +152,7 @@ CircularBufferQueue bufferQueue;
 uint32_t packet_number = 0;
 
 void setup1()
-{   // This runs on Core1.  It is the I2S setup.
+{ // This runs on Core1.  It is the I2S setup.
     Serial.begin();
     i2s.setDATA(2); // These are the pins for the data on the SDR-TRX
     i2s.setBCLK(0);
@@ -147,13 +167,13 @@ void setup1()
 }
 
 void loop()
-{   // This should run on Core0.  It is the UDP loop.
+{ // This should run on Core0.  It is the UDP loop.
     static BufferEmptyer emptyer(bufferQueue);
     emptyer.emptyBuffer(); // Empty the buffer
 }
 
 void loop1()
-{   // This should run on Core1.  It is the I2S loop.
+{ // This should run on Core1.  It is the I2S loop.
     static BufferFiller filler(bufferQueue);
     filler.fillBuffer(); // Fill the buffer
 }
