6170f5b802f449686136aa48c96bdc2c6f7423c5
frohro <rob.frohne@wallawalla.edu>
2024-04-17 21:57:02 +0000

    Apr 17, 2024, 2:57 PM

--- src/Multicore_Test_UDP_I2S.cpp
+++ src/Multicore_Test_UDP_I2S.cpp
@@ -141,12 +141,13 @@ public:
             // queue.moveToNextBuffer(false);
             // rp2040.resumeOtherCore();
             Serial.printf("Sent packet %d\n", *(int32_t *)buffer);
-            digitalWrite(18, LOW);
+            digitalWrite(16, LOW);
             digitalWrite(18, HIGH);
         }
         else
         {
             digitalWrite(16, HIGH);
+            digitalWrite(18, LOW);
         }
     }
 };
