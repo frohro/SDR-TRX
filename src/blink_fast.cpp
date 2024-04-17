7d2f249c8833b6fc2caa04addfb50a350cbf3789
frohro <rob.frohne@wallawalla.edu>
2024-04-17 20:42:54 +0000

    Apr 17, 2024, 1:42 PM

---
+++ src/blink_fast.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <si5351.h>
+
+const int LED_PIN = 16;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH);
+
+    digitalWrite(LED_PIN, LOW);
+
+}
