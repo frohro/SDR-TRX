60cd4206fcba8780239b141c84b73c5e870e77ec
frohro <rob.frohne@wallawalla.edu>
2024-04-17 22:32:13 +0000

    Apr 17, 2024, 3:32 PM

--- src/blink_fast.cpp
+++ src/blink_fast.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <si5351.h>
 
 const int SW_PIN = 16;
 const int SW1_PIN = 18;
@@ -16,12 +17,16 @@ void setup1()
 }
 
 void loop() {
+    rp2040.idleOtherCore();
     digitalWrite(SW_PIN, HIGH);
     digitalWrite(SW_PIN, LOW);
+    rp2040.resumeOtherCore();
 }
 
 void loop1()
 {
+    rp2040.idleOtherCore();
     digitalWrite(SW1_PIN, HIGH);
     digitalWrite(SW1_PIN, LOW);
+    rp2040.resumeOtherCore();   
 }
