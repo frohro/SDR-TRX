bcabdf14be99fe2ca7469c1bf7d37112a0b35a90
frohro <rob.frohne@wallawalla.edu>
2024-04-18 05:37:54 +0000

    Apr 17, 2024, 10:37 PM

--- src/Basic_Testing_UDP_Data_Stream.ino
+++ src/Basic_Testing_UDP_Data_Stream.ino
@@ -43,24 +43,23 @@ void loop()
     start_time = micros();
     if (mutex_try_enter(&my_mutex, &save))
     {
-        // Serial.println("Mutex entered Core0.");
+        Serial.println("Mutex entered Core0.");
+        udp.write((const uint8_t *)buffer, BUFFER_SIZE); // Takes about 810 us.
+        udp.endPacket();                                 // Takes about 670 us
+        int time_elapsed = micros() - start_time;
+        total_time += time_elapsed;
+        if (n++ == 5000)
+        {
+            Serial.printf("Time per statement: %f\n", (float)total_time / n);
+            start_time = micros();
+            total_time = 0;
+            n = 0;
+        }
         mutex_exit(&my_mutex);
     }
     else
     {
-        // Serial.println("Mutex not entered Core0.");
-    }
-    
-    udp.write((const uint8_t *)buffer, BUFFER_SIZE); // Takes about 810 us.
-    udp.endPacket();                                 // Takes about 670 us
-    int time_elapsed = micros() - start_time;
-    total_time += time_elapsed;
-    if (n++ == 5000)
-    {
-        Serial.printf("Time per statement: %f\n", (float)total_time / n);
-        start_time = micros();
-        total_time = 0;
-        n = 0;
+        Serial.println("Mutex not entered Core0.");
     }
 }
 
@@ -71,15 +70,15 @@ void setup1()
 
 void loop1()
 {
-    delay(100);
+    // delay(100);
     if (!mutex_try_enter(&my_mutex, &save))
     {
         Serial.println("Mutex not entered Core1.");
     }
     else
     {
-        // Serial.println("Mutex entered Core1.");
-        delay(10);
+        Serial.println("Mutex entered Core1.");
+        delay(1000);
         mutex_exit(&my_mutex);
     }
 }
