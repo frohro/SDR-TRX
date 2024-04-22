aae54e686604fc35f70c04c07cea02c4d814b88f
frohro <rob.frohne@wallawalla.edu>
2024-04-22 21:44:59 +0000

    Apr 22, 2024, 2:44 PM

--- Quisk_Files/test_UDP_file.py
+++ Quisk_Files/test_UDP_file.py
@@ -17,17 +17,25 @@ def main():
 
     # Prepare to receive packets
     packets = []
+    expected_packet_number = 0
 
     print("Listening for packets on port 12345...")
     while len(packets) < 4000:
         data, addr = sock.recvfrom(PACKET_SIZE)
         packet_number = struct.unpack('<I', data[:4])[0] # Little endian
+
+        # Check for missing or out-of-order packets
+        if packet_number != expected_packet_number:
+            print(f"Expected packet number {expected_packet_number}, but received packet number {packet_number}")
+        expected_packet_number += 1
+
         # Unpack the stereo audio data into a list of tuples (right, left)
         audio_data = struct.unpack('<366i', data[4:]) # Little endian
 
         # Pair up the integers as left and right audio samples
         audio_data_pairs = [(audio_data[i+1], audio_data[i]) for i in range(0, len(audio_data), 2)]
         packets.append((packet_number, audio_data_pairs))
+
     # Sort packets by packet number
     packets.sort(key=lambda x: x[0])
 
@@ -36,7 +44,6 @@ def main():
     audio_data_sorted = np.array([item for sublist in [item[1] for item in packets] for item in sublist], dtype=np.int32)
 
     # Write to a .wav file
-    # print(audio_data_pairs)
     with wave.open('output.wav', 'wb') as wav_file:
         wav_file.setnchannels(CHANNELS)
         wav_file.setsampwidth(4) # Assuming 16-bit samples
