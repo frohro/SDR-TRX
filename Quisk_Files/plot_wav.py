aff2b20b59573fdbfc29e3b18b8ed0d3c6caea07
frohro <rob.frohne@wallawalla.edu>
2024-04-23 03:28:13 +0000

    Apr 22, 2024, 8:28 PM

--- Quisk_Files/plot_wav.py
+++ Quisk_Files/plot_wav.py
@@ -12,8 +12,23 @@ audio_data = np.frombuffer(wav_file.readframes(nframes), dtype=np.int32)
 # Close the .wav file
 wav_file.close()
 
-# Plot the audio data in chunks of 1000 samples
-for i in range(0, len(audio_data), 1000):
+# Separate the audio data into left and right channels
+left_channel = audio_data[::2]
+right_channel = audio_data[1::2]
+
+# Calculate the sum and difference of the left and right channels
+sum_audio = left_channel + right_channel
+diff_audio = left_channel - right_channel
+
+# Plot the sum and difference audio data in chunks of 1000 samples
+for i in range(0, len(sum_audio), 1000):
     plt.figure(figsize=(10, 4))
-    plt.plot(audio_data[i:i+1000])
+    plt.plot(sum_audio[i:i+1000])
+    plt.title('Left + Right')
+    plt.show()
+
+for i in range(0, len(diff_audio), 1000):
+    plt.figure(figsize=(10, 4))
+    plt.plot(diff_audio[i:i+1000])
+    plt.title('Left - Right')
     plt.show()
