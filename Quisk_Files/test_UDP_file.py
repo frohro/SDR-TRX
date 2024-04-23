e3aa221986790f29b7a8f5d34c961abb4c03e6e0
frohro <rob.frohne@wallawalla.edu>
2024-04-23 04:02:20 +0000

    Apr 22, 2024, 9:02 PM

--- Quisk_Files/test_UDP_file.py
+++ Quisk_Files/test_UDP_file.py
@@ -34,18 +34,18 @@ def main():
 
     # Comment this if you are using 32 bit samples instead of 24.
     while len(packets) < 10000:
-    data, addr = sock.recvfrom(PACKET_SIZE)
-    packet_number = struct.unpack('<I', data[:4])[0] # Little endian
+        data, addr = sock.recvfrom(PACKET_SIZE)
+        packet_number = struct.unpack('<I', data[:4])[0] # Little endian
 
-    # Unpack the audio data into bytes
-    audio_data_bytes = struct.unpack('<' + 'B' * (PACKET_SIZE - 4), data[4:])
+        # Unpack the audio data into bytes
+        audio_data_bytes = struct.unpack('<' + 'B' * (PACKET_SIZE - 4), data[4:])
 
-    # Combine the bytes into integers
-    audio_data = [audio_data_bytes[i] + (audio_data_bytes[i+1] << 8) + (audio_data_bytes[i+2] << 16) for i in range(0, len(audio_data_bytes), 3)]
+        # Combine the bytes into integers
+        audio_data = [audio_data_bytes[i] + (audio_data_bytes[i+1] << 8) + (audio_data_bytes[i+2] << 16) for i in range(0, len(audio_data_bytes), 3)]
 
-    # Pair up the integers as left and right audio samples
-    audio_data_pairs = list(zip(audio_data[1::2], audio_data[::2]))
-    packets.append((packet_number, audio_data_pairs))
+        # Pair up the integers as left and right audio samples
+        audio_data_pairs = list(zip(audio_data[1::2], audio_data[::2]))
+        packets.append((packet_number, audio_data_pairs))
 
     # Sort packets by packet number
     packets.sort(key=lambda x: x[0])
