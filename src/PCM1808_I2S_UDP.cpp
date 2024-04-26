de0ef4425c8e1c0c825fb85c65e7b21551c3b1a5
frohro <rob.frohne@wallawalla.edu>
2024-04-26 05:36:03 +0000

    Apr 25, 2024, 10:36 PM

--- src/PCM1808_I2S_UDP.cpp
+++ src/PCM1808_I2S_UDP.cpp
@@ -91,7 +91,7 @@ BufferEmptyer::BufferEmptyer(CircularBufferQueue &q) : queue(q)
     }
     else if (RATE == 96000)
     {
-        DELAY_TIME = 0; 
+        DELAY_TIME = 1000; 
     }
     else
     {
