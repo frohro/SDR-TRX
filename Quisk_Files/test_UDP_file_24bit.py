7fd8a978dee2d98105f93638b2dbc2c7bb737ece
frohro <rob.frohne@wallawalla.edu>
2024-04-26 01:26:29 +0000

    Apr 25, 2024, 6:26 PM

--- Quisk_Files/test_UDP_file_24bit.py
+++ Quisk_Files/test_UDP_file_24bit.py
@@ -13,7 +13,7 @@ PORT = 12345
 PACKET_SIZE = 1468 # 4 bytes for packet number + 183 * 8 bytes for int32_t pairs
 SAMPLE_RATE =  96000 
 CHANNELS = 2 # Stereo
-NUM_PACKETS = 200
+NUM_PACKETS = 1000
 NUM_PLOT_POINTS = 1000
 
 lock = threading.Lock()
