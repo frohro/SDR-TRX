d1aa1220752e932bfb7aaa036c5f06964d9519fa
frohro <rob.frohne@wallawalla.edu>
2024-04-28 21:53:03 +0000

    Apr 28, 2024, 2:53 PM

--- src/PCM1808_I2S_UDP.cpp
+++ src/PCM1808_I2S_UDP.cpp
@@ -151,7 +151,7 @@ void BufferEmptyer::emptyBuffer()
         BufferEmptyer::addDebugMessage("Number of packets sent %d at %d.  Packet_number sent %d\n", 
                 number_of_packets_sent, millis(), packet_number_in_packet);
         number_of_packets_sent++;
-        if (number_of_packets_sent > 1000)
+        if (number_of_packets_sent > 10000)
         {
             BufferEmptyer::printDebugMessages();
         }
