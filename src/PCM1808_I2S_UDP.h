63a3352cdf86a488c8168af4a7e568af7af43e2e
frohro <rob.frohne@wallawalla.edu>
2024-05-10 05:48:19 +0000

    May 9, 2024, 10:48 PM

--- src/PCM1808_I2S_UDP.h
+++ src/PCM1808_I2S_UDP.h
@@ -10,7 +10,7 @@
 extern uint32_t mutex_save;
 extern mutex_t my_mutex; // Mutex for thread safety
 extern WiFiUDP udpData;
-extern IPAddress remoteIp;
+extern IPAddress quiskIp;
 extern const char* STASSID;
 extern const int BITS_PER_SAMPLE_SENT;
 
