23897f1412b55be34adf754890f1329718b49326
frohro <rob.frohne@wallawalla.edu>
2024-05-26 03:52:15 +0000

    May 25, 2024, 8:52 PM

--- Python Files/ image_rejection.py
+++ Python Files/ image_rejection.py
@@ -15,3 +15,4 @@ Q = 1.01j + 0.01  # A 1% error in real and imaginary parts
 image_rejection_ratio = calculate_image_rejection(I, Q)
 
 print("Image Rejection Ratio (dB):", image_rejection_ratio)
+# When I run this I get 43 dB for a 1% error in the real and imaginary parts of the signal which seems about right.
