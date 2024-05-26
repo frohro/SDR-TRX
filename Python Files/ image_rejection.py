a27bacd2606b67ee76e4a8f2d2f952a5d295fd7c
frohro <rob.frohne@wallawalla.edu>
2024-05-26 03:47:07 +0000

    May 25, 2024, 8:47 PM

--- Python Files/ image_rejection.py
+++ Python Files/ image_rejection.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-def calculate_image_rejection(u, v):
+def calculate_image_rejection(I, Q):
     # Calculate the image rejection ratio
-    image_rejection = 20*np.log10(np.real(u + np.conj(v))/np.imag(u + np.conj(v)))
+    image_rejection = 20*np.log10(np.abs(I_+ j1*Q)/np.abs(I - j1*Q))
     return image_rejection
 
 # Generate example u and v signals
 # u is rotating clockwise, v is rotating counter-clockwise
-u = 1 + 1j
-v = u*1.0*np.exp(1j*np.pi*0.01*180./np.pi)
+I = 1
+Q = 1.01j + 0.01
 
 # Calculate the image rejection ratio
-image_rejection_ratio = calculate_image_rejection(u, v)
+image_rejection_ratio = calculate_image_rejection(I, Q)
 
 print("Image Rejection Ratio (dB):", image_rejection_ratio)
